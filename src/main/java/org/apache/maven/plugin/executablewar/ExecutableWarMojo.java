40ed532317adae0bf0e307d80f95f04f95c2e038
Stig Kleppe-Jørgensen <from.github@nisgits.net>
2010-02-16 02:41:06 +0100

    Added example usage of MojoExecutor

--- src/main/java/org/apache/maven/plugin/executablewar/ExecutableWarMojo.java
+++ src/main/java/org/apache/maven/plugin/executablewar/ExecutableWarMojo.java
@@ -19,9 +19,14 @@ package org.apache.maven.plugin.executablewar;
  * under the License.
  */
 
+import org.apache.maven.execution.MavenSession;
+import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
-import org.apache.maven.plugin.war.WarMojo;
+import org.apache.maven.plugin.PluginManager;
+import org.apache.maven.project.MavenProject;
+import org.twdata.maven.mojoexecutor.MojoExecutor;
+import org.twdata.maven.mojoexecutor.MojoExecutor.Element;
 
 /**
  * Build an executable WAR file.
@@ -32,9 +37,51 @@ import org.apache.maven.plugin.war.WarMojo;
  * @phase package
  * @requiresDependencyResolution runtime
  */
-public class ExecutableWarMojo extends WarMojo {
+public class ExecutableWarMojo extends AbstractMojo {
+	/**
+     * The Maven Project Object
+     *
+     * @parameter expression="${project}"
+     * @required
+     * @readonly
+     */
+    protected MavenProject project;
+
+    /**
+     * The Maven Session Object
+     *
+     * @parameter expression="${session}"
+     * @required
+     * @readonly
+     */
+    protected MavenSession session;
+
+    /**
+     * The Maven PluginManager Object
+     *
+     * @component
+     * @required
+     */
+    protected PluginManager pluginManager;
+
 	public void execute() throws MojoExecutionException, MojoFailureException {
+		final Element element = MojoExecutor.element(MojoExecutor.name("outputDirectory"), "${project.build.directory}/foo");
+
+		MojoExecutor.executeMojo(
+				MojoExecutor.plugin(
+						          MojoExecutor.groupId("org.apache.maven.plugins"),
+                     MojoExecutor.artifactId("maven-dependency-plugin"),
+                     MojoExecutor.version("2.0")
+             ),
+             MojoExecutor.goal("copy-dependencies"),
+             MojoExecutor.configuration(new Element[]{element}),
+				MojoExecutor.executionEnvironment(
+                     project,
+                     session,
+                     pluginManager
+             )
+          );
+
 		System.out.println("RUNNING");
-		super.execute();
 	}
 }
