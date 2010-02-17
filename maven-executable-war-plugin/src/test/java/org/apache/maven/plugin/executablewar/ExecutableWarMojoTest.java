8464a12ea01c9fa0202070e849e44f466eb181ac
Stig Kleppe-Jørgensen <from.github@nisgits.net>
2010-02-17 21:53:12 +0100

    Disable test as integration testing is easier

--- maven-executable-war-plugin/src/test/java/org/apache/maven/plugin/executablewar/ExecutableWarMojoTest.java
+++ maven-executable-war-plugin/src/test/java/org/apache/maven/plugin/executablewar/ExecutableWarMojoTest.java
@@ -2,7 +2,9 @@ package org.apache.maven.plugin.executablewar;
 
 import java.io.File;
 
+import com.google.common.collect.Lists;
 import org.apache.maven.plugin.testing.AbstractMojoTestCase;
+import org.apache.maven.plugin.testing.stubs.ArtifactStub;
 
 /**
  * Unit test of {@link ExecutableWarMojo}
@@ -10,11 +12,22 @@ import org.apache.maven.plugin.testing.AbstractMojoTestCase;
  * @author Stig Kleppe-Jorgensen, 2010.02.16
  */
 public class ExecutableWarMojoTest extends AbstractMojoTestCase {
-	public void test_mojo_is_loaded() throws Exception {
-		final File testPom = new File(getBasedir(), "src/test/resources/empty-mojo.xml");
+	// TODO disabled for now; easier to just use integration tests 
+	public void tst_mojo_is_loaded() throws Exception {
+		final String pathToConfig = getClass().getResource("/empty-mojo.xml").getPath();
 
-		ExecutableWarMojo mojo = (ExecutableWarMojo) lookupMojo("war", testPom);
-
+		ExecutableWarMojo mojo = (ExecutableWarMojo) lookupMojo("war", new File(pathToConfig));
 		assertNotNull(mojo);
+
+		this.setVariableValueToObject(mojo, "pluginArtifacts", Lists.newArrayList(createArtifact()));
+		mojo.execute();
+	}
+
+	private ArtifactStub createArtifact() {
+		final ArtifactStub artifact = new ArtifactStub();
+		artifact.setArtifactId("maven-executable-war-library");
+		artifact.setGroupId("org.apache.maven.plugins");
+		
+		return artifact;
 	}
 }
