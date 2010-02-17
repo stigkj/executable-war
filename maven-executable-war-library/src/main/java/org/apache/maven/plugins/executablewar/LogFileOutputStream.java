8fa4b8a430e6cf266d6088c48ec03759e0aeb1da
Stig Kleppe-Jørgensen <from.github@nisgits.net>
2010-02-17 08:44:24 +0100

    Used in Main.java

--- maven-executable-war-library/src/main/java/org/apache/maven/plugins/executablewar/LogFileOutputStream.java
+++ maven-executable-war-library/src/main/java/org/apache/maven/plugins/executablewar/LogFileOutputStream.java
@@ -1,14 +1,14 @@
 package org.apache.maven.plugins.executablewar;
 
-import sun.misc.Signal;
-import sun.misc.SignalHandler;
-
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FilterOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+
+import sun.misc.Signal;
+import sun.misc.SignalHandler;
 
 /**
  * {@link OutputStream} that writes to a log file.
