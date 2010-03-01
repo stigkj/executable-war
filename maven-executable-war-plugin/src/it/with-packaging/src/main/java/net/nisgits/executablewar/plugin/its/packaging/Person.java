3600734bec953917fcd1612fe88720153ec1f2a9
Stig Kleppe-Jørgensen <from.github@nisgits.net>
2010-03-01 15:20:51 +0100

    Added mojo and components.xml for running the plugin under a packaging (executable-war) in addition to have it as a regular plugin

---
+++ maven-executable-war-plugin/src/it/with-packaging/src/main/java/net/nisgits/executablewar/plugin/its/packaging/Person.java
@@ -0,0 +1,16 @@
+package net.nisgits.executablewar.plugin.its.def;
+
+public class Person
+{
+    private String name;
+    
+    public void setName( String name )
+    {
+        this.name = name;
+    }
+    
+    public String getName()
+    {
+        return name;
+    }
+}
---
+++ maven-executable-war-plugin/src/main/java/net/nisgits/executablewar/plugin/ExecutableWarMojoInPackaging.java
@@ -0,0 +1,204 @@
+package net.nisgits.executablewar.plugin;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+import com.google.common.base.Function;
+import com.google.common.collect.Maps;
+import org.apache.maven.artifact.Artifact;
+import org.apache.maven.execution.MavenSession;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugin.PluginManager;
+import org.apache.maven.project.MavenProject;
+import org.codehaus.plexus.archiver.ArchiverException;
+import org.codehaus.plexus.archiver.UnArchiver;
+import org.codehaus.plexus.archiver.manager.ArchiverManager;
+import org.codehaus.plexus.archiver.manager.NoSuchArchiverException;
+import org.codehaus.plexus.components.io.fileselectors.FileInfo;
+import org.codehaus.plexus.components.io.fileselectors.FileSelector;
+import org.codehaus.plexus.util.FileUtils;
+
+import static org.twdata.maven.mojoexecutor.MojoExecutor.artifactId;
+import static org.twdata.maven.mojoexecutor.MojoExecutor.configuration;
+import static org.twdata.maven.mojoexecutor.MojoExecutor.element;
+import static org.twdata.maven.mojoexecutor.MojoExecutor.executeMojo;
+import static org.twdata.maven.mojoexecutor.MojoExecutor.executionEnvironment;
+import static org.twdata.maven.mojoexecutor.MojoExecutor.goal;
+import static org.twdata.maven.mojoexecutor.MojoExecutor.groupId;
+import static org.twdata.maven.mojoexecutor.MojoExecutor.name;
+import static org.twdata.maven.mojoexecutor.MojoExecutor.plugin;
+import static org.twdata.maven.mojoexecutor.MojoExecutor.version;
+
+/**
+ * Build an executable WAR file.
+ *
+ * @author <a href="from.executable-war@nisgits.net">Stig Kleppe-Jørgensen</a>
+ * @version $Id: $
+ * TODO make a JDK v1.4 version too with that maven plugin
+ *
+ * @goal war
+ * @phase package
+ */
+public class ExecutableWarMojoInPackaging extends AbstractMojo {
+	/**
+	 * The Maven Project Object
+	 *
+	 * @parameter expression="${project}"
+	 * @required
+	 * @readonly
+	 */
+	private MavenProject project;
+
+	/**
+	 * The Maven Session Object
+	 *
+	 * @parameter expression="${session}"
+	 * @required
+	 * @readonly
+	 */
+	private MavenSession session;
+
+	/**
+	 * The Maven PluginManager Object
+	 *
+	 * @component
+	 * @required
+	 */
+	private PluginManager pluginManager;
+
+	/**
+	 * The directory for the generated WAR.
+	 *
+	 * @parameter expression="${project.build.directory}"
+	 * @required
+	 */
+	private String buildDirectory;
+
+	/**
+	 * To look up Archiver/UnArchiver implementations
+	 *
+	 * @component
+	 */
+	private ArchiverManager archiverManager;
+
+	/**
+	 * The dependencies declared in your executablewar.
+	 *
+	 * @parameter default-value="${executablewar.artifacts}"
+	 */
+	private List<Artifact> pluginArtifacts;
+
+	/**
+	 * The name of the generated WAR.
+	 *
+	 * @parameter expression="${project.build.finalName}"
+	 * @required
+	 */
+	private String warName;
+
+	/**
+	 * Maps "groupId:artifactId" to the corresponding artifact.
+	 * Built from pluginArtifacts.
+	 */
+	private Map<String, Artifact> idToArtifact;
+
+	public void execute() throws MojoExecutionException, MojoFailureException {
+		verifyCorrectPackaging(project.getPackaging());
+
+		idToArtifact = mapIdToArtifact();
+		final File expandedWarDirectory = new File(buildDirectory, warName);
+
+		extractExecWarClassesTo(expandedWarDirectory);
+		copyDependenciesTo(expandedWarDirectory);
+
+		executeMojo(
+				plugin(
+						groupId("org.apache.maven.plugins"),
+						artifactId("maven-war-plugin"),
+						version("2.1-beta-1")
+				),
+				goal("war"),
+				configuration(
+						element(name("archive"),
+								element(name("manifest"),
+										element(name("mainClass"), "org.apache.maven.executablewar.Main")
+								)
+						)
+				),
+				executionEnvironment(
+						project,
+						session,
+						pluginManager
+				)
+		);
+	}
+
+	private void verifyCorrectPackaging(final String packaging) throws MojoFailureException {
+		if (!packaging.equals("executable-war")) {
+			throw new MojoFailureException(
+					"Can only be run within a project with 'executable-war' packaging, that is, when building an executable web application");
+		}
+	}
+
+	private Map<String, Artifact> mapIdToArtifact() {
+		return Maps.uniqueIndex(pluginArtifacts, new Function<Artifact, String>() {
+			public String apply(Artifact from) {
+				return from.getGroupId() + ":" + from.getArtifactId();
+			}
+		});
+	}
+
+	private void extractExecWarClassesTo(final File expandedWarDirectory) {
+		final Artifact artifact = idToArtifact.get("org.apache.maven.plugins:maven-executable-war-library");
+		final File artifactFile = artifact.getFile();
+
+		try {
+			final UnArchiver unArchiver = archiverManager.getUnArchiver(artifactFile);
+			unArchiver.setSourceFile(artifactFile);
+
+			expandedWarDirectory.mkdirs();
+			unArchiver.setFileSelectors(new FileSelector[]{new IsClassFileSelector()});
+			unArchiver.setDestDirectory(expandedWarDirectory);
+			unArchiver.extract();
+		} catch (NoSuchArchiverException e) {
+			throw new IllegalStateException("Could not get unarchiver for " + artifactFile, e);
+		} catch (ArchiverException e) {
+			throw new IllegalStateException("Could not extract " + artifactFile, e);
+		}
+	}
+
+	private void copyDependenciesTo(File expandedWarDirectory) throws MojoExecutionException {
+		copyArtifactByIdToDirectory("net.java.dev.jna:jna", expandedWarDirectory);
+		copyArtifactByIdToDirectory("com.sun.akuma:akuma", expandedWarDirectory);
+		copyArtifactByIdToDirectory("net.sourceforge.winstone:winstone", expandedWarDirectory);
+	}
+
+	private void copyArtifactByIdToDirectory(final String id, File expandedWarDirectory) throws MojoExecutionException {
+		final Artifact artifact = idToArtifact.get(id);
+		copyArtifactToDirectory(artifact, expandedWarDirectory);
+	}
+
+	private void copyArtifactToDirectory(Artifact artifact, File expandedWarDirectory) throws MojoExecutionException {
+		try {
+			FileUtils.copyFile(artifact.getFile(), new File(expandedWarDirectory, artifact.getArtifactId() + ".jar"));
+		} catch (FileNotFoundException e) {
+			throw new MojoExecutionException("Could not find file for artifact", e);
+		} catch (IOException e) {
+			throw new MojoExecutionException("Problems copying artifact", e);
+		}
+	}
+
+	/**
+	 * Selects only class files
+	 */
+	private static class IsClassFileSelector implements FileSelector {
+		public boolean isSelected(FileInfo fileInfo) throws IOException {
+			return fileInfo.isFile() && fileInfo.getName().endsWith(".class");
+		}
+	}
+}
