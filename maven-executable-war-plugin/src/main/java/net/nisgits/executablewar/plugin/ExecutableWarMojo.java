76ebde2c195ce74b2d4ee597d4c0dec47671eeec
Stig Kleppe-Jørgensen <from.github@nisgits.net>
2010-03-01 15:09:05 +0100

    Fixed an erroneous refactoring of a string

--- maven-executable-war-plugin/src/main/java/net/nisgits/executablewar/plugin/ExecutableWarMojo.java
+++ maven-executable-war-plugin/src/main/java/net/nisgits/executablewar/plugin/ExecutableWarMojo.java
@@ -28,7 +28,7 @@ import org.codehaus.plexus.util.FileUtils;
  *
  * @author <a href="from.executable-war@nisgits.net">Stig Kleppe-Jørgensen</a>
  * @version $Id: $
- * TODO make a JDK v1.4 version too with that maven executablewar
+ * TODO make a JDK v1.4 version too with that maven plugin
  * TODO need to check if we are in a war packaging and only run then...or?? What about other war-packaging variants
  * TODO improve name of goal
  *
