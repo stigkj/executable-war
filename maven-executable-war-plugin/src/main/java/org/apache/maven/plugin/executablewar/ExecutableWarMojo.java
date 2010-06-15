94c8fa58cb87a223715ec735537ef91cefc00de0
Stig Kleppe-Jørgensen <from.github@nisgits.net>
2010-06-15 16:04:17 +0200

    Forgot to fix the artifact id when doing artifact lookup inside mojo

--- maven-executable-war-plugin/src/main/java/org/apache/maven/plugin/executablewar/ExecutableWarMojo.java
+++ maven-executable-war-plugin/src/main/java/org/apache/maven/plugin/executablewar/ExecutableWarMojo.java
@@ -146,7 +146,8 @@ public class ExecutableWarMojo extends AbstractMojo {
 	}
 
 	private void extractExecWarClassesTo(final File expandedWarDirectory) {
-		final Artifact artifact = idToArtifact.get("net.nisgits.executablewar:maven-executable-war-library");
+        // LATER is it possible to not hard code these dependencies? 
+		final Artifact artifact = idToArtifact.get("net.nisgits.executablewar:executable-war-library");
 		final File artifactFile = artifact.getFile();
 
 		try {
